65ff60816afea74eec6c86e9b985da6be18129f8
AlanVargasUT <Alan16UT@gmail.com>
2019-11-08 20:08:18 -0600

    Cafeteria

---
+++ server.js
@@ -0,0 +1,31 @@
+require('./config/config');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const bodyParser = require('body-parser');
+
+//Parse aplication/x-www-form-urlencode
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Parse formato a aplication/json
+app.use(bodyParser.json());
+
+//Archivo agrupador de rutas
+app.use(require('./server/routes/index'));
+
+//Conexion a la base de datos
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+
+    },
+    (err, resp) => {
+        if (err) throw err;
+
+        console.log('Base de datos Online');
+    });
+
+//Puerto de escucha de la aplicacion
+app.listen(process.env.PORT, () => {
+    console.log("escuchando por el puerto: ", process.env.PORT);
+});
