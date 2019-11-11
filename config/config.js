7106b51f87180a99a69fd8479c84a892cc78f36e
AlanVargasUT <Alan16UT@gmail.com>
2019-11-11 09:41:11 -0600

    Agregando entornos de produccion y desarrollo

--- config/config.js
+++ config/config.js
@@ -1,2 +1,16 @@
 //PUERTO
 process.env.PORT = process.env.PORT || 3000;
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// CONEXION A LA BASE DE DATOS
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafeteria';
+} else {
+    urlDB = 'mongodb+srv://admin:aotqf86761@cluster0-e0vrc.mongodb.net/cafeteria';
+}
+
+process.env.URLDB = urlDB;
